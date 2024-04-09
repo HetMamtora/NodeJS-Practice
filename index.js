eaefeb987899562e85769dfb1a3323db4dcf9140
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-04-09 16:31:35 +0530

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 // hello();
 
-console.log("LOCAL MODULES");
+console.log("LOCAL MODULES : : ");
 //METHOD-1
 console.log("Method-1");
 require("./add");
@@ -20,4 +20,48 @@ console.log(sum2);
 
 
 console.log("")
-console.log("BUILT-IN MODULES");
+console.log("BUILT-IN  : : ");
+
+const OS = require('os');
+const a = OS.freemem();
+const b = OS.totalmem();
+const c = OS.userInfo();
+console.log(a,b,c);
+
+
+console.log("");
+console.log("Path Module");
+
+const path = require('path');
+console.log(__filename);
+
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+console.log(path.extname(__filename));
+
+console.log(path.join('/hello','/hello','file.txt'));
+
+console.log("");
+console.log("File Structure Module");
+console.log("Asynchronous Way")
+
+const fs = require('fs');
+fs.readFile('file.txt','utf-8',(err,data) => {
+    if(err){
+        console.log(err);
+        return
+    }
+    console.log(data);
+})
+
+console.log("Synchronous Way")
+fs.writeFile('text.txt','Writting in text file',(err) => {
+    if(err){
+        console.log(err);
+        return
+    }
+    console.log('File written: Success');
+})
+
+fs.writeFileSync('text2.txt','Inside text 2 writting', 'utf-8')
+console.log('File Written: Success');
